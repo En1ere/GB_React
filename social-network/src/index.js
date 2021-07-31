37d6bd4b86231546e191f6f736969e7b70fcee7f
Alexander <62078595+En1ere@users.noreply.github.com>
2021-08-01 03:28:40 +0500

    1

--- social-network/src/index.js
+++ social-network/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
-import "./styles/global.scss";
+import { App } from "./components/app/App";
+import "./style/global.scss";
 
 ReactDOM.render(
   <React.StrictMode>
